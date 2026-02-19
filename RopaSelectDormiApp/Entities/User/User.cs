6abf4198807806eed3c7ed756ce2c73bee4901f4
Luis Daniel Nuñez Villegas <dnunezv0@gmail.com>
2026-02-19 14:00:30 -0600

    Exclude and delete files

--- RopaSelectDormiApp/Entities/User/User.cs
+++ RopaSelectDormiApp/Entities/User/User.cs
@@ -1,11 +1,8 @@
-using System;
-using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace RopaSelectDormiApp.Entities.User;
 
-using RopaSelectDormiApp.Entities.UserRole;
-
 public class User: IdentityUser<Guid>
 {
+    
 }
